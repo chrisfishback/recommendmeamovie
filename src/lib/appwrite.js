405981834d2f8532d396fac7c322f49b2786d53f
Chris Fishback <chrisfishback@Chriss-MacBook-Pro.local>
2025-11-16 17:22:42 -0600

    remove comments

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -3,7 +3,7 @@ import { Client } from 'appwrite';
 const client = new Client();
 
 client
-  .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite endpoint
-  .setProject('YOUR_PROJECT_ID'); // Your project ID
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject('YOUR_PROJECT_ID');
 
   export { client };
